c6e1db376261d1524c990066a68aadeecc293fdf
Abhishek Gupta <141378237+AbhishekGuptaHub@users.noreply.github.com>
2023-09-01 21:57:58 +0530

    Create Reverse_String.java

---
+++ Day_1/Reverse_String.java
@@ -0,0 +1,19 @@
+class Solution
+{
+    public void reverseString(char [] s)
+    {
+        int low = 0, high = s.length - 1;
+
+        char temp;
+
+        while (low < high)
+        {
+            temp = s[low];
+            s[low] = s[high];
+            s[high] = temp;
+
+            low++;
+            high--;
+        }
+    }
+}
