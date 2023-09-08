d6015431a5a500b7a5f5fa82d7e09c7441d18862
ABHISHEK GUPTA <141378237+AbhishekGuptaHub@users.noreply.github.com>
2023-09-08 15:05:59 +0530

    Create ValidAnagram.java

---
+++ Day_8/ValidAnagram.java
@@ -0,0 +1,30 @@
+class Solution
+{
+    public boolean isAnagram(String s, String t)
+    {
+        if (s.length() != t.length())
+        {
+            return false;
+        }
+
+        int [] freq = new int[26];
+
+        Arrays.fill(freq, 0);
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            freq[s.charAt(i) - 97]++;
+            freq[t.charAt(i) - 97]--;
+        }
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (freq[i] != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
